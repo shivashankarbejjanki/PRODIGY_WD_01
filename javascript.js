76d77c68e19d498ac6563207491cd002ec90a849
shivashankarbejjanki <162316443+shivashankarbejjanki@users.noreply.github.com>
2024-07-28 23:54:36 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// JavaScript to change the header background on scroll
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
